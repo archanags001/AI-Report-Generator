98941b0278cc5a9e0c4fad40e6ff223b5a89ca0f
Archana G S <62071221+archanags001@users.noreply.github.com>
2025-08-08 18:46:51 -0700

    Update streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -226,11 +226,11 @@ if uploaded_file:
                                 #         unsafe_allow_html=True
                                 #     )
 
-                                display_pdf(pdf_file_content)
+                                # display_pdf(pdf_file_content)
 
-                                base64_pdf = base64.b64encode(pdf_file_content).decode("utf-8")
-                                pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="600" type="application/pdf"></iframe>'
-                                st.markdown(pdf_display, unsafe_allow_html=True)
+                                # base64_pdf = base64.b64encode(pdf_file_content).decode("utf-8")
+                                # pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="600" type="application/pdf"></iframe>'
+                                # st.markdown(pdf_display, unsafe_allow_html=True)
                             
                                 
                             else:
